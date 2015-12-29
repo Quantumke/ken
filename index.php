e09d1dd037f859512d1a4774fff81a0672010f0b
Benson Mburu <nguruben@gmail.com>
2015-12-29 20:41:52 +0300

    greetingd

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+
 <?php
  $greetings = ['Hi there', 'Hello human', 'hello'];
 $relay_greetings = $greetings[mt_rand(0, count($greetings) - 1)];
-echo $relay_greetings;
+
+?>
+
+
+<?php
+
+$str=  "Hello ken hi";
+$count = substr_count($str, "hi");
+echo $count;
+if ( $count  == 1)
+  {
+echo $relay_greetings;	
+}
+
+
+
 ?>
